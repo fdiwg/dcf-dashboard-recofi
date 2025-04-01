6989915979a7617626e98b065e7cdaf263efca73
Bennici <alexandre-ben@hotmail.fr>
2025-04-01 09:22:21 +0200

    hidden data download

--- server.R
+++ server.R
@@ -4,12 +4,33 @@ server <- function(input, output,session) {
   #---------------------------------------------------------------------------------------
   dcf.dashboard::dcf_dashboard_server(
     input = input, output = output, session = session,
-    tasks = c(
-      "Task: Catch"="catch",
-      "Task: Effort"="effort"
+    tasks =  list(
+      list(
+        id = "catch",
+        label = "Task: Catch",
+        formats = list(
+          list(
+            id = "simplified",
+            label="Simplified",
+            ref= "https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_simplified_recofi_catch.json"
+          )
+        )
+      ),
+      list(
+        id = "effort",
+        label = "Task: Effort",
+        formats = list(
+          list(
+            id = "simplified",
+            label="Simplified",
+            ref= "https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_simplified_recofi_effort.json"
+          )
+        )
+      )
     ),
     reporting_entity = c("Flagstates" = "flagstate"),
-    ref_reporting_entities = "https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/regional/recofi/cl_flagstate.csv"
+    ref_reporting_entities = "https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/regional/recofi/cl_flagstate.csv",
+    allow_download=FALSE
   )
 
 }
